0a6483c68fe4127bbd754ab991cc5c0c4cd3a06e
marko drnasin <60597312+Drnasin208@users.noreply.github.com>
2022-09-03 20:37:46 +0200

    Delete animate-on-scroll.js

--- Scroll-Animate on Scroll/js/animate-on-scroll.js
+++
@@ -1,5 +0,0 @@
-AOS.init({
-  offset: 500, // offset (in px) from the original trigger point
-  delay: 100, // values from 0 to 3000, with step 50ms
-  duration: 1500, // values from 0 to 3000, with step 50ms
-});
